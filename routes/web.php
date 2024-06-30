0a283484e6b78f511510a5608d386d3731f0d7c3
shinnkura <n.shinzo1010@icloud.com>
2024-06-30 18:37:12 +0900

    controllerの作成

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UtilityController;
+use App\Http\Controllers\GameController;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -11,3 +13,12 @@ Route::get('/hello', fn() => view("hello", ["name" => "俺", "age" => "20"]));
 
 Route::get('/', fn() => view("index"));
 Route::get('/curriculum', fn() => view("curriculum"));
+
+// 世界の時間
+Route::get('/world-time', [UtilityController::class, 'worldTime']);
+
+// おみくじ
+Route::get('/omikuji', [GameController::class, 'omikuji']);
+
+// モンティ・ホール問題
+Route::get('/monty-hall', [GameController::class, 'montyHall']);
