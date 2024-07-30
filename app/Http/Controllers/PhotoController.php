d34847405956351b43e51dcd663f467a831425cb
shinnkura <n.shinzo1010@icloud.com>
2024-07-30 17:55:01 +0900

    コメント追加

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -20,6 +20,10 @@ class PhotoController extends Controller
      */
     public function store(Request $request)
     {
+        // ファイルの保存
+        // Requestクラスのfileメソッドで、
+        // storeメソッドの第一引数は、保存先のディレクトリを指定
+        // 第二引数は、ドライバー名を指定（ドライバーは「ディスク」ともいい、ファイルの管理方法のことを言います。）
         $saveFilePath = $request->file('image')->store('photos', 'public');
         Log::debug('ファイルパス：' . $saveFilePath);
 
